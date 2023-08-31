7c94bc9dd15cfb3fdab14118595498c06031da90
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2023-08-31 04:43:48 -0700

    solved a couple katas

---
+++ src/Alphanumeric.java
@@ -0,0 +1,16 @@
+public class Alphanumeric {
+    public static boolean alphanumeric(String s) {
+        String alphanumerals = "abcdefghijklmnopqrstuvwxyz1234567890";
+        if(s.length() == 0) {
+            return false;
+        } else {
+            String[] sArr = s.split("");
+            for (int i = 0; i < sArr.length; i++) {
+                if (!alphanumerals.contains(sArr[i].toLowerCase())) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-//        MathEvaluator mathEvaluator = new MathEvaluator();
-        //Part 1 Tests:
-//        System.out.println("Test 1: Positives and negatives, just addition:");
-//
-//        System.out.println("1a: " + (mathEvaluator.calculate("1 + 1") == 2.0 ? "✅" : "❌"));
-//        System.out.println("1b: " + (mathEvaluator.calculate("1 - 1") == 0.0 ? "✅" : "❌"));
-//        System.out.println("1c: " + (mathEvaluator.calculate("1 + -1") == 0.0 ? "✅" : "❌"));
-//        System.out.println("1d: " + (mathEvaluator.calculate("1 - -1") == 2.0 ? "✅" : "❌"));
-//
-//        System.out.println("1e: " + (mathEvaluator.calculate("-1 + 1") == 0.0 ? "✅" : "❌"));
-//        System.out.println("1f: " + (mathEvaluator.calculate("-1 - 1") == -2.0 ? "✅" : "❌"));
-//        System.out.println("1g: " + (mathEvaluator.calculate("-1 + -1") == -2.0 ? "✅" : "❌"));
-//        System.out.println("1h: " + (mathEvaluator.calculate("-1 - -1") == 0.0 ? "✅" : "❌"));
-//
-//        System.out.println("1i: " + (mathEvaluator.calculate("2 + 2") == 4.0 ? "✅" : "❌"));
-//        System.out.println("1j: " + (mathEvaluator.calculate("4 - 4") == 0.0 ? "✅" : "❌"));
-//        System.out.println("1k: " + (mathEvaluator.calculate("5 + -1") == 4.0 ? "✅" : "❌"));
-//        System.out.println("1l: " + (mathEvaluator.calculate("6 - -3") == 9.0 ? "✅" : "❌"));
-//
-//        System.out.println("1m: " + (mathEvaluator.calculate("-9 + 1") == -8.0 ? "✅" : "❌"));
-//        System.out.println("1n: " + (mathEvaluator.calculate("-6 - 4") == -10.0 ? "✅" : "❌"));
-//        System.out.println("1o: " + (mathEvaluator.calculate("-5 + -2") == -7.0 ? "✅" : "❌"));
-//        System.out.println("1p: " + (mathEvaluator.calculate("-2 - -7") == 5.0 ? "✅" : "❌"));
-//
-//        System.out.println("1q: " + (mathEvaluator.calculate("-15 + -20") == -35 ? "✅" : "❌"));
-//        System.out.println("1r: " + (mathEvaluator.calculate("2 - 70") == -68 ? "✅" : "❌"));
-
-        System.out.println(EasyKata.getStatus(false));
+        System.out.println(BitCounting.countBits(0) == 0 ? "✅" : "❌");
+        System.out.println(BitCounting.countBits(1) == 1 ? "✅" : "❌");
+        System.out.println(BitCounting.countBits(2) == 1 ? "✅" : "❌");
+        System.out.println(BitCounting.countBits(3) == 2 ? "✅" : "❌");
+        System.out.println(BitCounting.countBits(4) == 1 ? "✅" : "❌");
+        System.out.println(BitCounting.countBits(5) == 2 ? "✅" : "❌");
+        System.out.println(BitCounting.countBits(6) == 2 ? "✅" : "❌");
+        System.out.println(BitCounting.countBits(7) == 3 ? "✅" : "❌");
+        System.out.println(BitCounting.countBits(8) == 1 ? "✅" : "❌");
+        System.out.println(BitCounting.countBits(15) == 4 ? "✅" : "❌");
     }
 }
 
