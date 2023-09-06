b6207d898057dddae38dceb77f2d3d0280b09639
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2023-09-05 22:09:17 -0700

    basic factorial

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        char[] chars = new char[]{'e', 'e', 'e', 'e', 'e', 'w', 'w', 'w', 'w', 'w'};
-        System.out.println(TenMinWalk.isValid(chars));
+        System.out.println("Group 1: Basic factorials");
+        System.out.println("1. " + (TrailingZerosOfFactorial.factorial(-2) == -1 ? "✅" : "❌"));
+        System.out.println("2. " + (TrailingZerosOfFactorial.factorial(-10) == -1 ? "✅" : "❌"));
+        System.out.println("3. " + (TrailingZerosOfFactorial.factorial(0) == 1 ? "✅" : "❌"));
+        System.out.println("4. " + (TrailingZerosOfFactorial.factorial(1) == 1 ? "✅" : "❌"));
+        System.out.println("5. " + (TrailingZerosOfFactorial.factorial(2) == 2 ? "✅" : "❌"));
+        System.out.println("6. " + (TrailingZerosOfFactorial.factorial(3) == 6 ? "✅" : "❌"));
+        System.out.println("7. " + (TrailingZerosOfFactorial.factorial(4) == 24 ? "✅" : "❌"));
+        System.out.println("8. " + (TrailingZerosOfFactorial.factorial(5) == 120 ? "✅" : "❌"));
+        System.out.println("9. " + (TrailingZerosOfFactorial.factorial(6) == 720 ? "✅" : "❌"));
+        System.out.println("10. " + (TrailingZerosOfFactorial.factorial(7) == 5040 ? "✅" : "❌"));
+        System.out.println("11. " + (TrailingZerosOfFactorial.factorial(8) == 40320 ? "✅" : "❌"));
+        System.out.println("12. " + (TrailingZerosOfFactorial.factorial(9) == 362880 ? "✅" : "❌"));
+        System.out.println("13. " + (TrailingZerosOfFactorial.factorial(10) == 3628800 ? "✅" : "❌"));
+        System.out.println("14. " + (TrailingZerosOfFactorial.factorial(11) == 39916800 ? "✅" : "❌"));
+        System.out.println("15. " + (TrailingZerosOfFactorial.factorial(12) == 479001600 ? "✅" : "❌"));
     }
 }
-
--- src/TrailingZerosOfFactorial.java
+++ src/TrailingZerosOfFactorial.java
@@ -1,6 +1,9 @@
 public class TrailingZerosOfFactorial {
     public static int factorial(int n) {
-        if(n > 1) {
+        if(n < 0) {
+            System.out.println("n is negative, therefore factorial is undefined");
+            return -1;
+        } else if(n > 1) {
             return n * factorial(n - 1);
         } else {
             return 1;
