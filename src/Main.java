ef9fc9053ead1a98330a956cf0e30f93bf710e70
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2023-09-07 08:59:15 -0700

    solved trailingZerosFactorial kata

--- src/Main.java
+++ src/Main.java
@@ -33,5 +33,23 @@ public class Main {
         System.out.println("13. " + (TrailingZerosOfFactorial.countDigits(10) == 7 ? "✅" : "❌"));
         System.out.println("14. " + (TrailingZerosOfFactorial.countDigits(11) == 8 ? "✅" : "❌"));
         System.out.println("15. " + (TrailingZerosOfFactorial.countDigits(12) == 9 ? "✅" : "❌"));
+
+        System.out.println("\nGroup 3: trailing zeros");
+        System.out.println("1. " + (TrailingZerosOfFactorial.zeros(0) == 0 ? "✅" : "❌"));
+        System.out.println("2. " + (TrailingZerosOfFactorial.zeros(1) == 0 ? "✅" : "❌"));
+        System.out.println("3. " + (TrailingZerosOfFactorial.zeros(5) == 1 ? "✅" : "❌"));
+        System.out.println("4. " + (TrailingZerosOfFactorial.zeros(10) == 2 ? "✅" : "❌"));
+        System.out.println("5. " + (TrailingZerosOfFactorial.zeros(12) == 2 ? "✅" : "❌"));
+        System.out.println("6. " + (TrailingZerosOfFactorial.zeros(20) == 4 ? "✅" : "❌"));
+        System.out.println("7. " + (TrailingZerosOfFactorial.zeros(25) == 6 ? "✅" : "❌"));
+        System.out.println("8. " + (TrailingZerosOfFactorial.zeros(50) == 12 ? "✅" : "❌"));
+        System.out.println("9. " + (TrailingZerosOfFactorial.zeros(76) == 18 ? "✅" : "❌"));
+        System.out.println("10. " + (TrailingZerosOfFactorial.zeros(100) == 24 ? "✅" : "❌"));
+        System.out.println("11. " + (TrailingZerosOfFactorial.zeros(120) == 28 ? "✅" : "❌"));
+        System.out.println("12. " + (TrailingZerosOfFactorial.zeros(124) == 28 ? "✅" : "❌"));
+        System.out.println("13. " + (TrailingZerosOfFactorial.zeros(125) == 31 ? "✅" : "❌"));
+        System.out.println("14. " + (TrailingZerosOfFactorial.zeros(200) == 49 ? "✅" : "❌"));
+        System.out.println("15. " + (TrailingZerosOfFactorial.zeros(625) == 156 ? "✅" : "❌"));
+
     }
 }
--- src/TrailingZerosOfFactorial.java
+++ src/TrailingZerosOfFactorial.java
@@ -12,4 +12,15 @@ public class TrailingZerosOfFactorial {
     public static int countDigits(int n) {
         return n >= 0 ? String.valueOf(factorial(n)).length() : 0;
     }
+
+    //only needed this method for kata
+    public static int zeros(int n) {
+        int z = 0;
+        int i = 1;
+        while(n >= Math.pow(5, i)) {
+            z += Math.floor(n / Math.pow(5, i));
+            i++;
+        }
+        return z;
+    }
 }
