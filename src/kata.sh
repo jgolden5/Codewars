85141b4ed57f6d8d878d85f97593bc1f4ed967c7
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2023-09-18 15:11:20 -0700

    solved ParityOutlier (java) & isValid (bash)

--- src/kata.sh
+++ src/kata.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-printerError() {
-  colorsArray=$( echo $1 | grep -o . )
-  numberOfBadColors=0
-  for color in ${colorsArray[@]}; do
-    if [[ "nopqrstuvwxyz" == *"$color" ]]; then
-      numberOfBadColors=$(($numberOfBadColors + 1))
+isValid() {
+  isValid=True
+
+  if [[ $1 = *" "* ]]; then
+    isValid=False
+  else
+    IFS='.' read -r -a adrArr <<< "$1"
+
+    dotCount=0
+
+    for ip in ${adrArr[@]}; do
+      dotCount=$((dotCount+1))
+      if [[ ${ip/0/} ]] && [[ ${ip::1} -eq 0 ]] || [[ $ip -lt 0 ]] || [[ $ip -gt 255 ]]; then
+        isValid=False
+        break
+      fi
+    done
+
+    if [[ $dotCount -ne 4 ]]; then
+      isValid=False
     fi
-  done
-  errorString="$numberOfBadColors/${#colorsArray[@]}"
-  echo $errorString
-	echo "Echo is working"
+  fi
+
+
+  echo $isValid
 }
