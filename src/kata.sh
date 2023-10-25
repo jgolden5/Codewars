a4063411582d8dff84b1d69fa7d30c55517d7833
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2023-10-25 13:36:47 -0700

    intToBin works

--- src/kata.sh
+++ src/kata.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
 
+#int to binary
+intToBin() {
+  remaining=$1
+  if [[ $remaining -le 0 ]]; then
+    echo 0
+  else
+    exp=0
+    while [[ $((2**$exp)) -le $1 ]]; do
+      exp=$((exp+1))
+    done
+
+    exp=$((exp-1))
+
+    bin=""
+
+    while [[ $exp -ge 0 ]]; do
+      currentOneValue=$((2**$exp))
+      if [[ $remaining -ge $currentOneValue ]]; then
+        remaining=$(($remaining - $currentOneValue))
+        bin+=1
+      else
+        bin+=0
+      fi
+      exp=$((exp - 1))
+    done
+
+    echo $bin
+
+  fi
+}
+
 n=$1
 
-echo $n
+b="$(intToBin $n)"
+
+
+
+
+echo $b
+
