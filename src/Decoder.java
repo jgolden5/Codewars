30674a0ff49505d3d6e1f73450d1cc669ffa7983
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-04-29 16:36:22 -0700

    file contents get accurately printed based on file input

--- src/Decoder.java
+++ src/Decoder.java
@@ -1,7 +1,15 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
 public class Decoder {
-    public String decode(File messageFile) {
-        String[] lines = messageFile.toString().split("\\d+");
+    public String decode(File messageFile) throws IOException {
+        ArrayList<String> lines = (ArrayList<String>) Files.readAllLines(messageFile.toPath(), Charset.defaultCharset());
+        for(String line : lines) {
+            System.out.println(line);
+        }
+        return null;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-
+        File f1 = new File("/Users/jgolden1/example.txt");
         Decoder decoder = new Decoder();
-
+        try {
+            decoder.decode(f1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
