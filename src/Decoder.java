eec7cbb56b73dfbc8515a3ce7a791aaf45e08024
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-04-29 18:10:26 -0700

    setup for current solution strategy

--- src/Decoder.java
+++ src/Decoder.java
@@ -3,27 +3,41 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Decoder {
 
     public String decode(File messageFile) throws IOException {
         ArrayList<String> fileLines = (ArrayList<String>) Files.readAllLines(messageFile.toPath(), Charset.defaultCharset());
-        ArrayList<Integer> pyramidLineEndIndices = getPyramidLineEndIndices(fileLines.size());
-        HashMap<Integer, String> numberToContent = new HashMap<>();
-        return null;
+        ArrayList<String> pyramidLineEndIndices = generateEndIndices(fileLines.size());
+        String[] unsortedDecodedLines = calculateDecodedLines(pyramidLineEndIndices);
+        String[] sortedDecodedLines = sortDecodedLines(unsortedDecodedLines);
+        return getDecodedMessage(sortedDecodedLines);
     }
 
-    public ArrayList<Integer> getPyramidLineEndIndices(int fileLength) {
-        ArrayList<Integer> pyramidLineEndIndices = new ArrayList<>();
+    public ArrayList<String> generateEndIndices(int fileLength) {
+        ArrayList<String> pyramidLineEndIndices = new ArrayList<>();
         int n = 1;
         int interval = 2;
         while(n <= fileLength) {
-            pyramidLineEndIndices.add(n);
+            pyramidLineEndIndices.add(Integer.toString(n));
             n += interval;
             interval++;
         }
         return pyramidLineEndIndices;
+    }
+
+    private String[] calculateDecodedLines(ArrayList<String> pyramidLineEndIndices) {
+        return null;
+    }
+
+    private String[] sortDecodedLines(String[] unsortedDecodedLines) {
+        return null;
+    }
+
+    private String getDecodedMessage(String[] decodedLinesInOrder) {
+        return null;
     }
 
 }
