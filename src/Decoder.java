4022681f61a5bc95bf205f40809630d406f1bd78
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-04-29 17:15:36 -0700

    getPyramidLineEndIndicesWorks

--- src/Decoder.java
+++ src/Decoder.java
@@ -3,13 +3,27 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Decoder {
+
     public String decode(File messageFile) throws IOException {
-        ArrayList<String> lines = (ArrayList<String>) Files.readAllLines(messageFile.toPath(), Charset.defaultCharset());
-        for(String line : lines) {
-            System.out.println(line);
-        }
+        ArrayList<String> fileLines = (ArrayList<String>) Files.readAllLines(messageFile.toPath(), Charset.defaultCharset());
+        ArrayList<Integer> pyramidLineEndIndices = getPyramidLineEndIndices(fileLines.size());
+        HashMap<Integer, String> numberToContent = new HashMap<>();
         return null;
     }
+
+    public ArrayList<Integer> getPyramidLineEndIndices(int fileLength) {
+        ArrayList<Integer> pyramidLineEndIndices = new ArrayList<>();
+        int n = 1;
+        int interval = 2;
+        while(n <= fileLength) {
+            pyramidLineEndIndices.add(n);
+            n += interval;
+            interval++;
+        }
+        return pyramidLineEndIndices;
+    }
+
 }
