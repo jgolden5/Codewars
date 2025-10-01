a44eb118b94d9a8949ddbd42a0735ba3c0191ea4
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-09-30 21:03:20 -0700

    solved reverse words kata in Go
    .

--- src/Codewars/EasyKata.java
+++ src/Codewars/EasyKata.java
@@ -1,14 +1,41 @@
 package Codewars;
 
+import java.util.ArrayList;
+
 public class EasyKata {
 
-    public static long findNb(long m) {
-        double s = Math.sqrt(m);
-        if(s == Math.floor(s)) {
-            return (long)s;
-        } else {
-            return -1;
-        }
+    public static ArrayList<Integer> getOrderedEvens(ArrayList<Integer> randomInts) {
+      ArrayList<Integer> evenInts = extractEvens(randomInts);
+      return bubbleSort(evenInts);
     }
+
+  private static ArrayList<Integer> bubbleSort(ArrayList<Integer> evenInts) {
+    for(int i = 0; i < evenInts.size() - 1; i++) {
+      int left = evenInts.get(i);
+      int right = evenInts.get(i + 1);
+      if(left > right) {
+        swapElements(evenInts, i);
+      }
+    }
+    return evenInts;
+  }
+
+  private static ArrayList<Integer> extractEvens(ArrayList<Integer> randomInts) {
+    ArrayList<Integer> evenInts = new ArrayList<>();
+    for(int r : randomInts) {
+      if(r % 2 == 0) {
+        evenInts.add(r);
+      }
+    }
+    return evenInts;
+  }
+
+  //mutating func
+  private static void swapElements(ArrayList<Integer> original, int i) {
+    int right = original.remove(i);
+    int left = original.remove(i);
+    original.add(i, right);
+    original.add(i, left);
+  }
 
 }
--- src/Codewars/Main.java
+++ src/Codewars/Main.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-      MathEvaluator math = new MathEvaluator();
-      System.out.println("prove it works here (once it works): ");
+      ArrayList<Integer> unorderedRandomInts = new ArrayList<>();
+      unorderedRandomInts.add(3);
+      unorderedRandomInts.add(4);
+      unorderedRandomInts.add(2);
+      unorderedRandomInts.add(9);
+      unorderedRandomInts.add(10);
+      unorderedRandomInts.add(1);
+      unorderedRandomInts.add(0);
+      System.out.println(EasyKata.getOrderedEvens(unorderedRandomInts));
     }
 }
