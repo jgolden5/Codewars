c0d2e0e586759a04ea7b3977e04018f05980993f
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-10-05 08:41:42 -0700

    solved LeapYear kata in go

---
+++ GoKata/leapYear.go
@@ -0,0 +1,8 @@
+package main
+
+func IsLeapYear(year int) bool {
+  divisibleBy4 := year % 4 == 0
+  divisibleBy100 := year % 100 == 0
+  divisibleBy400 := year % 400 == 0
+  return divisibleBy4 && (!divisibleBy100 || divisibleBy400)
+}
--- GoKata/main.go
+++ GoKata/main.go
@@ -6,12 +6,16 @@ import (
 )
 
 func main() {
-  var w1 string = "test"
-  var w2 string = "dolphin"
-  var w3 string = "alaska"
-  var w4 string = "step-father"
-  fmt.Println(bandNameGenerator(w1))
-  fmt.Println(bandNameGenerator(w2))
-  fmt.Println(bandNameGenerator(w3))
-  fmt.Println(bandNameGenerator(w4))
+  y1 := 2000
+  y2 := 2100
+  y3 := 1999
+  y4 := 2024
+  y5 := 2300
+  y6 := 2400
+  fmt.Println(y1, IsLeapYear(y1))
+  fmt.Println(y2, IsLeapYear(y2))
+  fmt.Println(y3, IsLeapYear(y3))
+  fmt.Println(y4, IsLeapYear(y4))
+  fmt.Println(y5, IsLeapYear(y5))
+  fmt.Println(y6, IsLeapYear(y6))
 }
