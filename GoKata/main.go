03230a3a6d6e9b5873f0c5c9915e5bc6bfbd1581
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-10-05 10:32:04 -0700

    isPrime go solution works functionally but needs optimization rwb
    .
    
    .

--- GoKata/main.go
+++ GoKata/main.go
@@ -6,7 +6,36 @@ import (
 )
 
 func main() {
-  var f1 Fighter = Fighter{Name: "Bob", Health: 35, DamagePerAttack: 12}
-  var f2 Fighter = Fighter{Name: "Joe", Health: 45, DamagePerAttack: 7}
-  fmt.Println("The winner is...", DeclareWinner(f1, f2, f2.Name))
+  n1 := 1
+  n2 := 2
+  n3 := 3
+  n4 := 4
+  n5 := 5
+  n6 := 6
+  n7 := 7
+  n8 := 8
+  n9 := 9
+  n10 := 10
+  n11 := 11
+  n12 := 12
+  n13 := 100000
+  n14 := 11211131
+  n15 := 1100000321
+  n16 := 2147483648
+  fmt.Println(n1,IsPrime(n1))
+  fmt.Println(n2,IsPrime(n2))
+  fmt.Println(n3,IsPrime(n3))
+  fmt.Println(n4,IsPrime(n4))
+  fmt.Println(n5,IsPrime(n5))
+  fmt.Println(n6,IsPrime(n6))
+  fmt.Println(n7,IsPrime(n7))
+  fmt.Println(n8,IsPrime(n8))
+  fmt.Println(n9,IsPrime(n9))
+  fmt.Println(n10,IsPrime(n10))
+  fmt.Println(n11,IsPrime(n11))
+  fmt.Println(n12,IsPrime(n12))
+  fmt.Println(n13,IsPrime(n13))
+  fmt.Println(n14,IsPrime(n14))
+  fmt.Println(n15,IsPrime(n15))
+  fmt.Println(n16,IsPrime(n16))
 }
